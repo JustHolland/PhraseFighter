e796c9586e752638c0938b12a4f00dc61c4d480d
justineholland <justineholland2@gmail.com>
2021-12-13 13:33:46 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log("hi");
+
+//creates random words and returns it
+function makeHardWords() {
+  let words = ""
+  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  for ( i = 0; i < 10; i++)
+    words += letters.charAt(Math.floor(Math.random() * letters.length));
+
+  return words;
+}
+
+console.log(makeHardWords(10));

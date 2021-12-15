7cf20247ea8ed1e596c85b9ea0d44db8001cef96
justineholland <justineholland2@gmail.com>
2021-12-15 12:25:30 -0600

    player buttons work

--- js/app.js
+++ js/app.js
@@ -12,8 +12,51 @@ function makeHardWords() {
 
 console.log(makeHardWords(10));
 
+
+// let nameR =
+// let nameB =
+
+
 class KANG{
   constructor(name, health, punchpoint,) {
+    this.name =name
+    this.health= health
+    this.punchpoint =punchpoint
+
 
   }
 }
+
+const solobtn = document.querySelector("#one")
+solobtn.addEventListener("click", soloSelected);
+
+function soloSelected(){
+  document.getElementById("numPlayers").style.display = "none";
+}
+
+
+const twobtn = document.querySelector("#two")
+twobtn.addEventListener("click", twoSelected);
+
+function twoSelected(){
+document.getElementById("numPlayers").style.display = "none";
+}
+
+
+// function showNamePlayersBox{
+//
+// }
+
+// function getPlayers() {
+//   let player1 = document.getElementById('bname').value;
+//   let player2 = document.getElementById('rname').value;
+//   alert(player1)
+
+
+
+// const feed = document.querySelector('#feedbtn')
+//   // console.log(life started)
+// feed.addEventListener('click', (event) => {
+//   event.target.clicked = true
+//   game.feedDino()
+//    document.getElementById("foodhide").style.display = "block";

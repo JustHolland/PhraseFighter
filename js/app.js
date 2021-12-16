a9f399b2debba3512adc689844c43afe0a11f1ac
justineholland <justineholland2@gmail.com>
2021-12-16 09:49:22 -0600

    random phrases print to screen

--- js/app.js
+++ js/app.js
@@ -1,62 +1,84 @@
 console.log("hi");
 
+
 //creates random words and returns it
-function makeHardWords() {
-  let words = ""
-  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  for ( i = 0; i < 10; i++)
-    words += letters.charAt(Math.floor(Math.random() * letters.length));
+function createRandomPhrase() {
+  let phraseArr = ['Quality time' , 'The plot thickens' , 'Cut to the chase', 'Burst your bubble' , 'Birds of a feather flock together' , 'Back to the drawing board', 'Hard pill to swallow'
+, 'Dropping like flies' , 'Down and out' , 'Cut the mustard' , 'Down to earth', 'Cry over spilt milk' , 'You cant judge a book by its cover' , 'Swinging for the fences',];
+  let phrase =""
+     phrase += phraseArr[Math.floor(Math.random() * phraseArr.length)];
+  return phrase;
 
-  return words;
 }
 
-console.log(makeHardWords(10));
+
+//add new phrase to screen
+document.getElementById("phrasebox").innerHTML = createRandomPhrase();
 
 
-// let nameR =
-// let nameB =
 
 
-class KANG{
-  constructor(name, health, punchpoint,) {
-    this.name =name
-    this.health= health
-    this.punchpoint =punchpoint
+// GAME SETUP ///
 
 
-  }
-}
-
+//ONE PLAYER//
 const solobtn = document.querySelector("#one")
 solobtn.addEventListener("click", soloSelected);
 
 function soloSelected(){
   document.getElementById("numPlayers").style.display = "none";
+  createRandomPhrase();
 }
 
-
+//TWO PLAYERS//
 const twobtn = document.querySelector("#two")
 twobtn.addEventListener("click", twoSelected);
 
 function twoSelected(){
 document.getElementById("numPlayers").style.display = "none";
+createRandomPhrase();
 }
 
 
-// function showNamePlayersBox{
-//
+
+
+
+//ROUND TIMER//
+let timeLeft = 30;
+let clocktimer = document.getElementById('timer');
+
+
+// start when user hits enter on the text input box
+  let timerId = setInterval(countdown, 1000);
+    function countdown() {
+      if (timeLeft >-1) {
+        clocktimer.innerHTML = timeLeft +'s remaining';
+        timeLeft--;
+
+      }
+
+    }
+
+
+//typing game/////
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("typebox").addEventListener("keyup", function(event) {
+//     if (event.keyCode === 13) {
+//     	document.getElementById("typebox").submit();
+// 		return false;
+//     }
+// });
+
+// function evaluatePlayerText{
+//   playertext = input_area.value;
+// curr_input_array = curr_input.split('');
 // }
-
-// function getPlayers() {
-//   let player1 = document.getElementById('bname').value;
-//   let player2 = document.getElementById('rname').value;
-//   alert(player1)
-
-
-
-// const feed = document.querySelector('#feedbtn')
-//   // console.log(life started)
-// feed.addEventListener('click', (event) => {
-//   event.target.clicked = true
-//   game.feedDino()
-//    document.getElementById("foodhide").style.display = "block";
